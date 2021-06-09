d11fe68a8050390d23e77b0a55e616c197765541
Marwam Malta <marwamprog.js@gmail.com>
2021-06-08 21:25:48 -0300

    Exercicio 2 - Até o 2

--- curso-java/src/br/com/cursojava/exercicio_2/Exec1.java
+++ curso-java/src/br/com/cursojava/exercicio_2/Exec1.java
@@ -1,9 +1,28 @@
+/*
+1. Faça um Programa que peça dois números e imprima o maior deles.
+*/
+
 package br.com.cursojava.exercicio_2;
+
+import java.util.Scanner;
 
 public class Exec1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe o primeiro número inteiro.: ");
+		int num1 = scan.nextInt();
+		
+		System.out.println("Informe o segundo número inteiro.: ");
+		int num2 = scan.nextInt();
+		
+		if(num1 > num2) {
+			System.out.println("O primeiro numero (" + num1 + ") é maior que o segundo numero (" + num2 + ")");
+		} else {
+			System.out.println("O segundo numero (" + num2 + ") é maior que o primeiro numero (" + num1 + ")");
+		}
 
 	}
 
---
+++ curso-java/src/br/com/cursojava/exercicio_2/Exec2.java
@@ -0,0 +1,28 @@
+/*
+2. Faça um Programa que peça um valor e mostre na tela se o valor é
+positivo ou negativo.
+*/
+
+
+package br.com.cursojava.exercicio_2;
+
+import java.util.Scanner;
+
+public class Exec2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe um número inteiro.: ");
+		int num = scan.nextInt();
+		
+		if(num % 2 == 0) {
+			System.out.println("O número " + num + " é PAR");
+		} else {
+			System.out.println("O número " + num + " é IMPAR");
+		}
+
+	}
+
+}
